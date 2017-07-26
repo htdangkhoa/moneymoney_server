7d98d87f78c91c6f02dca69420d2b17138f0104f
DangKhoa <dangkhoa@DangKhoas-MacBook-Pro.local>
2017-07-26 21:11:31 +0700

    Add load balancing using cluster

--- server.js
+++ server.js
@@ -2,11 +2,29 @@ require("dotenv").config();
 require("./app/variables");
 require("./app/models/user");
 require("./app/models/record");
+require("./app/models/note");
 require("./app/passport/passport");
 require("./app/middlewares");
 
-let app = global.variables.app;
+let app = global.variables.app,
+    cluster = require("cluster")
+    numCPUs = require("os").cpus().length;
 
-app.listen(process.env.PORT, () => {
+if (cluster.isMaster) {
     console.log("Server is running on port:", process.env.PORT);
-})
+    console.log("Master " + process.pid + " is running");
+
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on("exit", (worker, code, signal) => {
+        console.log('worker %d died (%s). restarting...', worker.process.pid, signal || code);
+
+        cluster.fork();
+    });
+}else {
+    app.listen(process.env.PORT, () => {
+        console.log("Worker " + process.pid + " started");
+    });
+}
