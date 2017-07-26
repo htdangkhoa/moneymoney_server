3c5561b8917d2a1ff1efb522c43a5532d95e31a4
DangKhoa <dangkhoa@DangKhoas-MacBook-Pro.local>
2017-07-26 23:19:30 +0700

    New API get info of user and edit info of user

--- app/routes/authentication.js
+++ app/routes/authentication.js
@@ -8,7 +8,7 @@ router.all("/", (req, res) => {
     )  return global.errorHandler(res, 401, "Unauthorized.");
 
     return global.successHandler(res, 200, "Signed in.");
-})
+});
 
 /**
  * @function sign_in
@@ -23,7 +23,7 @@ router.all("/", (req, res) => {
  */
 router.post("/sign_in", passport.authenticate("local", { failureRedirect: "/fail" }), (req, res) => {
     return res.redirect("/");
-})
+});
 
 /**
  * @function register
@@ -73,10 +73,84 @@ router.post("/sign_out", (req, res) => {
     req.session.destroy();
     console.log(req.session);
     return res.redirect("/");
-})
+});
+
+/**
+ * @function get_info
+ * @instance
+ * @param {string} email Email of user (Required).
+ * @example <caption>Requesting /info?email=abc@gmail.com with the following GET data.</caption>
+ */
+router.get("/info", (req, res) => {
+    var email = req.param("email");
+
+    if (
+        !req.user
+    ) return res.redirect("/");
+
+    if (
+        global.isEmpty(email, null)
+    ) return global.errorHandler(res, 400, "Bad request.");
+
+    User
+    .findOne({
+        email
+    }, ["email", "name", "cards"])
+    .then(user => {
+        if (!user) return global.errorHandler(res, 404, "This email does not exist.");
+
+        return global.successHandler(res, 201, user);
+    })
+    .catch(error => {
+        return global.errorHandler(res, 200, error);
+    });
+});
+
+/**
+ * @function edit_info
+ * @instance
+ * @param {string} name Name of user (Required).
+ * @param {string} email Email of user (Required).
+ * @example <caption>Requesting /info with the following PUT data.</caption>
+ * {
+ *  title: "",
+ *  content: "",
+ *  id: "59789c0db2638003d2712f95"
+ * }
+ */
+router.put("/info", (req, res) => {
+    var email = req.body.email,
+        name = req.body.name;
+
+    if (
+        !req.user
+    ) return res.redirect("/");
+
+    if (
+        global.isEmpty(email, null) || 
+        global.isEmpty(name, null)
+    ) return global.errorHandler(res, 400, "Bad request.");
+
+    User
+    .findOneAndUpdate({
+        email
+    }, {
+        $set: {
+            name
+        }
+    })
+    .then(user => {
+        if (!user) return global.errorHandler(res, 404, "This email does not exist.");
+
+        return global.successHandler(res, 200, "Your info was updated successfully.");
+    })
+    .catch(error => {
+        return global.errorHandler(res, 200, error);
+    });
+});
 
 router.get("/fail", (req, res) => {
     return global.errorHandler(res, 200, "Something went wrong.");
-})
+});
 
 module.exports = router;
